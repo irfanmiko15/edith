24308a48a613fb0837e2911303f8dc956fb9ae80
Irfan Dary <irfandarys@gmail.com>
2023-06-21 10:51:15 +0700

    Initial Commit

---
+++ edith/edithApp.swift
@@ -0,0 +1,17 @@
+//
+//  edithApp.swift
+//  edith
+//
+//  Created by Irfan Dary Sujatmiko on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct edithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
