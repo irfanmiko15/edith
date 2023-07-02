0da73fd48162ca2ced9b73fa03af67a6f3dcd4e7
mluthfi3d <mluthfi3d@gmail.com>
2023-07-02 20:02:12 +0700

    stage 1 85%

--- edith/Component/ContainerView.swift
+++ edith/Component/ContainerView.swift
@@ -10,45 +10,85 @@ import SwiftUI
 struct ContainerView: View {
     var textInside: String = "Testing teks banyak"
     var strokeWidth: CGFloat = CGFloat(20)
+    var width = CGFloat(600)
+    var height = CGFloat(300)
+    var textSize: Int = 24
+    var ratio = 0.001
     
     var body: some View{
-        GeometryReader {reader in
-            ZStack{
-                VStack{
-                    Path { path in
-                        path.move(to: CGPoint(x: 0+(reader.size.width*0.05), y: 0))
-                        path.addLine(to: CGPoint(x: reader.size.width-(reader.size.width*0.05), y: 0))
-                        path.addCurve(
-                            to: CGPoint(x: reader.size.width, y: 0+(reader.size.width*0.05)),
-                            control1: CGPoint(x: reader.size.width-(reader.size.width*0.05), y:0+(reader.size.width*0.03)),
-                            control2: CGPoint(x: reader.size.width-(reader.size.width*0.03), y:0+(reader.size.width*0.05)))
-                        
-                        path.addLine(to: CGPoint(x: reader.size.width, y: reader.size.height-(reader.size.width*0.05)))
-                        path.addCurve(
-                            to: CGPoint(x: reader.size.width-(reader.size.width*0.05), y: reader.size.height),
-                            control1: CGPoint(x: reader.size.width, y:reader.size.height),
-                            control2: CGPoint(x: reader.size.width, y:reader.size.height))
-                        
-                        path.addLine(to: CGPoint(x: 0+(reader.size.width*0.05), y: reader.size.height))
-                        path.addCurve(
-                            to: CGPoint(x: 0, y: reader.size.height-(reader.size.width*0.05)),
-                            control1: CGPoint(x: 0, y:reader.size.height),
-                            control2: CGPoint(x: 0, y:reader.size.height))
-                        path.addLine(to: CGPoint(x: 0, y: 0+(reader.size.width*0.05)))
-                        
-                        path.addCurve(
-                            to: CGPoint(x: 0+(reader.size.width*0.05), y: 0),
-                            control1: CGPoint(x: 0, y:0),
-                            control2: CGPoint(x: 0, y:0))
-                    }
-                    .stroke(Color.orangeFox50, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round, lineJoin: .round))
-                    .foregroundColor(Color.white)
+        ZStack(alignment: .center){
+            VStack{
+                Path { path in
+                    path.move(to: CGPoint(x: 0+(width*0.05), y: 0))
+                    path.addLine(to: CGPoint(x: width-(width*0.1), y: 0))
+                    path.addCurve(
+                        to: CGPoint(x: width, y: 0+(width*0.1)),
+                        control1: CGPoint(x: width-(width*0.1), y:0+(width*0.1)-(width*0.025)),
+                        control2: CGPoint(x: width-(width*0.1)+(width*0.025), y:0+(width*0.1)))
+                    
+                    path.addLine(to: CGPoint(x: width, y: height-(width*0.05)))
+                    path.addCurve(
+                        to: CGPoint(x: width-(width*0.05), y: height),
+                        control1: CGPoint(x: width, y:height-(width*ratio)),
+                        control2: CGPoint(x: width-(width*ratio), y:height))
+                    
+                    path.addLine(to: CGPoint(x: 0+(width*0.05), y: height))
+                    path.addCurve(
+                        to: CGPoint(x: 0, y: height-(width*0.05)),
+                        control1: CGPoint(x: 0+(width*ratio), y:height),
+                        control2: CGPoint(x: 0, y:height-(width*ratio)))
+                    path.addLine(to: CGPoint(x: 0, y: 0+(width*0.05)))
+                    
+                    path.addCurve(
+                        to: CGPoint(x: 0+(width*0.05), y: 0),
+                        control1: CGPoint(x: 0, y:0+(width*ratio)),
+                        control2: CGPoint(x: 0+(width*ratio), y:0))
+                    path.closeSubpath()
                 }
-                Text(textInside)
-                
+                .fill(Color.white)
             }
-            .ignoresSafeArea(.all)
+            .frame(width: width, height: height)
+            
+            Text(textInside)
+                .frame(width: width-(width*0.3), height: height)
+                .offset(x: 0-(width*0.1)/2, y: 0-(height/3))
+                .multilineTextAlignment(.center)
+                .font(.custom(Font.balooBold, size: CGFloat(textSize)))
+            
+            
+            VStack{
+                Path { path in
+                    path.move(to: CGPoint(x: 0+(width*0.05), y: 0))
+                    path.addLine(to: CGPoint(x: width-(width*0.1), y: 0))
+                    path.addCurve(
+                        to: CGPoint(x: width, y: 0+(width*0.1)),
+                        control1: CGPoint(x: width-(width*0.1), y:0+(width*0.1)-(width*0.025)),
+                        control2: CGPoint(x: width-(width*0.1)+(width*0.025), y:0+(width*0.1)))
+                    
+                    path.addLine(to: CGPoint(x: width, y: height-(width*0.05)))
+                    path.addCurve(
+                        to: CGPoint(x: width-(width*0.05), y: height),
+                        control1: CGPoint(x: width, y:height-(width*ratio)),
+                        control2: CGPoint(x: width-(width*ratio), y:height))
+                    
+                    path.addLine(to: CGPoint(x: 0+(width*0.05), y: height))
+                    path.addCurve(
+                        to: CGPoint(x: 0, y: height-(width*0.05)),
+                        control1: CGPoint(x: 0+(width*ratio), y:height),
+                        control2: CGPoint(x: 0, y:height-(width*ratio)))
+                    path.addLine(to: CGPoint(x: 0, y: 0+(width*0.05)))
+                    
+                    path.addCurve(
+                        to: CGPoint(x: 0+(width*0.05), y: 0),
+                        control1: CGPoint(x: 0, y:0+(width*ratio)),
+                        control2: CGPoint(x: 0+(width*ratio), y:0))
+                    path.closeSubpath()
+                }
+                .stroke(Color.orangeFox70, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round, lineJoin: .round))
+            }
+            .frame(width: width, height: height)
         }
+        .ignoresSafeArea(.all)
     }
 }
 
--- edith/View/Stages/Stage1View.swift
+++ edith/View/Stages/Stage1View.swift
@@ -11,47 +11,165 @@ struct Stage1View: View {
     @State var listImages: [InteractiveImageModel] = []
     @State var resultsParents: [InteractiveImageModel] = []
     @State var resultsChild: [InteractiveImageModel] = []
-    @State var listImagesName = ["stage1Apple", "stage1Car", "stage1Laptop", "stage1TeddyBear", "stage1Tee", "stage1Veggie", "stage1Water"]
+    @State var listImagesName = ["stage1Apple", "stage1Water", "stage1Tee", "stage1Car", "stage1TeddyBear", "stage1Veggie", "stage1Laptop"]
+    
+    @State var isDragging = false
+    @State var draggedObject = InteractiveImageModel(image: "stage1Apple", x: CGFloat(0), y: CGFloat(0))
     
     @ObservedObject var stageViewModel: StageViewModel
     var body: some View {
         NavigationStack{
             GeometryReader {reader in
-                VStack{
-                    ZStack{
-                        ContainerView(textInside: "Ini", strokeWidth: CGFloat(10))
-                            .padding([.horizontal], 48)
-                            .padding([.vertical], 16)
-                        HStack{
+                ZStack{
+                    Image("bgModul")
+                        .resizable()
+                        .scaledToFill()
+                    VStack{
+                        ZStack(alignment: .center){
+                            ContainerView(textInside: "Pindahkan 4 benda yang menurut kalian merupakan \nkebutuhan ke zona kalian masing - masing.", strokeWidth: CGFloat(16), width: reader.size.width*0.9, height: reader.size.height*0.5*0.95)
+                                .offset(x: reader.size.width*0.03)
                             ForEach(listImages) { image in
-                                NavigationLink(destination: Stage2View(stage2: StageModel(prompt: [""], listImage: [], resultParent: [], resultChild: []), stageViewModel: StageViewModel())){
-                                    
-                                    Image(image.image)
-                                        .resizable()
-                                        .scaledToFit()
-                                }
+                                
+                                Image(image.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: reader.size.width/10)
+                                    .position(CGPoint(x: image.x, y: image.y))
+                                    .offset(x: reader.size.width*0.03)
+                                    .shadow(radius: 5)
+                                    .gesture(DragGesture()
+                                        .onChanged { value in
+                                            draggedObject.image = image.image
+                                            draggedObject.x = value.location.x
+                                            draggedObject.y = value.location.y
+                                            isDragging = true
+                                        }
+                                        .onEnded { value in
+                                            if(value.location.x < reader.size.width/2 && value.location.x > 0 && value.location.y > reader.size.height/2 && resultsChild.count < 4){
+                                                
+                                                let x = reader.size.width*0.2 +  reader.size.width*(CGFloat(Int(resultsChild.count)%2))/8
+                                                var y = CGFloat(0)
+                                                if(resultsChild.count < 2){
+                                                    y = reader.size.height*0.55 + CGFloat(reader.size.height*1/8)
+                                                } else {
+                                                    y = reader.size.height*0.6 + CGFloat(reader.size.height*2/8)
+                                                }
+                                            
+                                                resultsChild.append(InteractiveImageModel(image: draggedObject.image, isCorrect: false, x: x, y: y))
+                                            } else if(value.location.x > reader.size.width/2 && value.location.x < reader.size.width && value.location.y > reader.size.height/2 && resultsParents.count < 4){
+                                                
+                                                let x = reader.size.width*0.6 +  reader.size.width*(CGFloat(Int(resultsParents.count)%2))/8
+                                                var y = CGFloat(0)
+                                                if(resultsParents.count < 2){
+                                                    y = reader.size.height*0.55 + CGFloat(reader.size.height*1/8)
+                                                } else {
+                                                    y = reader.size.height*0.6 + CGFloat(reader.size.height*2/8)
+                                                }
+                                                
+                                                resultsParents.append(InteractiveImageModel(image: draggedObject.image, isCorrect: false, x: x, y: y))
+                                            }
+                                            isDragging = false
+                                        }
+                                    )
                             }
-                        }
-                    }.frame(height: reader.size.height/2)
+                            Image("stageEdithHead")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: reader.size.width/10, height: reader.size.width/10)
+                                .foregroundColor(.white)
+                                .background(Color.white).clipShape(Circle()).padding(8)
+                                .clipShape(Circle()).overlay(Circle().stroke(Color.orangeFox70,lineWidth: 24)).background(Color.white).clipShape(Circle())
+                                .position(x: reader.size.width*0.1, y: reader.size.height*0.25)
+                            
+                        }.frame(height: reader.size.height/2)
+                        
+                        HStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 32)
+                                    .fill(Color.blueTang95)
+                                    Text("ini punya anak")
+                                RoundedRectangle(cornerRadius: 32)
+                                    .stroke(Color.white, style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+                            }
+                            .padding([.leading, .vertical], 32)
+                            .padding([.trailing], 16)
+                            .frame(width: reader.size.width/2)
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 32)
+                                    .fill(Color.redWood95)
+                                Text("ini punya anak")
+                                RoundedRectangle(cornerRadius: 32)
+                                    .stroke(Color.white, style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+                            }
+                            .padding([.trailing, .vertical], 32)
+                            .padding([.leading], 16)
+                            .frame(width: reader.size.width/2)
+                        }.frame(height: reader.size.height/2)
+                    }
+                    if(isDragging){
+                        Image(draggedObject.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: reader.size.width/10)
+                            .position(CGPoint(x: draggedObject.x, y: draggedObject.y))
+                    }
+                    ForEach(resultsChild) { child in
+                        Image(child.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: reader.size.width/10)
+                            .position(CGPoint(x: child.x, y: child.y))
+                            .offset(x: reader.size.width*0.03)
+                            .shadow(radius: 5)
+//                            .gesture(DragGesture()
+//                                .onChanged { value in
+//                                    draggedObject.image = image.image
+//                                    draggedObject.x = value.location.x
+//                                    draggedObject.y = value.location.y
+//                                    isDragging = true
+//                                }
+//                                .onEnded { value in
+//                                    isDragging = false
+//                                }
+//                            )
+                    }
                     
-                    HStack{
-                        VStack{
-                            Text("ini punya bapak")
-                        }.frame(width: reader.size.width/2)
-                        VStack{
-                            Text("ini punya anak")
-                        }.frame(width: reader.size.width/2)
-                    }.frame(height: reader.size.height/2)
+                    
+                    ForEach(resultsParents) { parent in
+                        Image(parent.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: reader.size.width/10)
+                            .position(CGPoint(x: parent.x, y: parent.y))
+                            .offset(x: reader.size.width*0.03)
+                            .shadow(radius: 5)
+//                            .gesture(DragGesture()
+//                                .onChanged { value in
+//                                    draggedObject.image = image.image
+//                                    draggedObject.x = value.location.x
+//                                    draggedObject.y = value.location.y
+//                                    isDragging = true
+//                                }
+//                                .onEnded { value in
+//                                    isDragging = false
+//                                }
+//                            )
+                    }
+                }
+                .navigationBarBackButtonHidden(true)
+                .ignoresSafeArea(.all)
+                
+                .task {
+                    for (index, image) in listImagesName.enumerated() {
+                        let x = reader.size.width*0.2 + CGFloat(reader.size.width*CGFloat(Int(index))/10)
+                        let y = reader.size.height*0.25 + CGFloat(reader.size.height*CGFloat((Int(index)+1)%2)/8)
+                        listImages.append(InteractiveImageModel(image: image, isCorrect: false, x: CGFloat(x), y: y))
+                    }
+                    //                stage1 = StageModel(prompt: ["oke", "ok"], listImage: listImages, resultParent: resultsParents, resultChild: resultsChild)
+                    //                stageViewModel.save(stages: [stage1])
                 }
             }
             
-            .task {
-                for image in listImagesName {
-                    listImages.append(InteractiveImageModel(image: image, isCorrect: false, x: CGFloat(0), y: CGFloat(0)))
-                }
-//                stage1 = StageModel(prompt: ["oke", "ok"], listImage: listImages, resultParent: resultsParents, resultChild: resultsChild)
-//                stageViewModel.save(stages: [stage1])
-            }
         }
         
     }
