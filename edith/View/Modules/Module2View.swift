b6dffb8813a3487c3d0009d96739e9319626c63d
Evelyn <evie7911@gmail.com>
2023-07-04 11:02:03 +0700

    adjustment

---
+++ edith/View/Modules/Module2View.swift
@@ -0,0 +1,20 @@
+//
+//  Module2View.swift
+//  edith
+//
+//  Created by Evelyn Megawati Tiffany on 03/07/23.
+//
+
+import SwiftUI
+
+struct Module2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Module2View_Previews: PreviewProvider {
+    static var previews: some View {
+        Module2View()
+    }
+}
