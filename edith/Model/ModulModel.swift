3132cac977830bd1cfae498e7d9d5e3c9b2e2a8f
Evelyn <evie7911@gmail.com>
2023-07-07 10:01:44 +0700

    progressing modul

--- edith/Model/ModulModel.swift
+++ edith/Model/ModulModel.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct ModulModel:Identifiable, Hashable, Codable{
     let id=UUID()
-    let prompt:String
-    let edithImage:String
+    let prompt:[String]
+    let edithImage: [String]
     var listImage:[InteractiveImageModel]=[]
     
-    init(prompt: String, edithImage: String, listImage: [InteractiveImageModel]) {
+    init(prompt: [String], edithImage: [String], listImage: [InteractiveImageModel]) {
         self.prompt = prompt
         self.edithImage = edithImage
         self.listImage = listImage
